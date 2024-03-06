31876fdcda757425b6706d934f497431fcc6881d
diptesh17 <deorediptesh1883@gmail.com>
2024-03-06 14:45:12 +0530

    Added hero & infoboxes section

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-
         <main>{children}</main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const Properties = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome </h1>
-      <Link href="/properties">Show Properties</Link>
+      <Hero />
+      <InfoBoxes/>
     </div>
   );
 };
