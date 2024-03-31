9fb5666b6532dec694e1c5c71d22833e24c11009
diptesh17 <deorediptesh1883@gmail.com>
2024-03-31 19:32:32 +0530

    Added Oauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata: Metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -18,12 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
---
+++ utils/authOptions.ts
@@ -0,0 +1,33 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // Invoked on  successful signin
+    async signIn({ profile }) {
+      // 1.Connect to database
+      // 2.Check if user exists
+      // 3.If not, then add to database
+      // 4.Return true to allow sign in
+    },
+
+    // Modifies the session object
+    async session({ session }) {
+      // 1.Get user from database
+      // 2.Assign the user id to session
+      // 3.Return session
+    },
+  },
+};
