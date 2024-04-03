1f770977f065d18c84c352ba6d0f27b670e69a7c
diptesh17 <deorediptesh1883@gmail.com>
2024-04-03 10:42:58 +0530

    Completed Delete a Property  functionality

--- app/api/properties/[id]/route.ts
+++ app/api/properties/[id]/route.ts
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // Get : api/properties/:id
 export const GET = async (
@@ -16,6 +17,46 @@ export const GET = async (
     }
 
     return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
+// Delete : api/properties/:id
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const propertyId = params.id;
+
+    const sessionUser = await getSessionUser();
+
+    // check for session
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    await connectDB();
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) {
+      return new Response("Property  Not Found", { status: 404 });
+    }
+
+    // verify
+    if (property.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await property.deleteOne();
+    return new Response("Property Deleted", {
       status: 200,
     });
   } catch (error) {
