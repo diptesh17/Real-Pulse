5674639966c4a8fa35457a83f298ac5c91a6d40f
diptesh17 <deorediptesh1883@gmail.com>
2024-04-12 12:11:15 +0530

    Fix in authOptions.ts

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,9 +1,67 @@
+// import connectDB from "@/config/database";
+// import User from "@/models/User";
+
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       authorization: {
+//         params: {
+//           prompt: "consent",
+//           access_type: "offline",
+//           response_type: "code",
+//         },
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     // Invoked on successful signin
+//     async signIn({ profile }) {
+//       // 1. Connect to database
+//       await connectDB();
+//       // 2. Check if user exists
+//       const userExists = await User.findOne({ email: profile.email });
+//       // 3. If not, then add user to database
+//       if (!userExists) {
+//         // Truncate user name if too long
+//         const username = profile.name.slice(0, 20);
+
+//         await User.create({
+//           email: profile.email,
+//           username,
+//           image: profile.picture,
+//         });
+//       }
+//       // 4. Return true to allow sign in
+//       return true;
+//     },
+//     // Modifies the session object
+//     async session({ session }) {
+//       // 1. Get user from database
+//       const user = await User.findOne({ email: session.user.email });
+//       // 2. Assign the user id to the session
+//       session.user.id = user._id.toString();
+//       // 3. return session
+//       return session;
+//     },
+//   },
+// };
+
 import connectDB from "@/config/database";
 import User from "@/models/User";
-
 import GoogleProvider from "next-auth/providers/google";
+import {
+  NextAuthOptions,
+  User as NextAuthUser,
+  Account,
+  Profile,
+  AdapterUser,
+} from "next-auth";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -19,31 +77,52 @@ export const authOptions = {
   ],
   callbacks: {
     // Invoked on successful signin
-    async signIn({ profile }) {
+    async signIn({
+      user,
+      account,
+      profile,
+    }: {
+      user: NextAuthUser | AdapterUser;
+      account: Account | null;
+      profile: Profile | undefined;
+    }) {
       // 1. Connect to database
       await connectDB();
+
       // 2. Check if user exists
-      const userExists = await User.findOne({ email: profile.email });
+      const userExists = await User.findOne({ email: profile?.email });
+
       // 3. If not, then add user to database
       if (!userExists) {
         // Truncate user name if too long
-        const username = profile.name.slice(0, 20);
+        const username = profile?.name?.slice(0, 20) || "";
 
         await User.create({
-          email: profile.email,
+          email: profile?.email,
           username,
-          image: profile.picture,
+          image: profile?.picture,
         });
       }
+
       // 4. Return true to allow sign in
       return true;
     },
     // Modifies the session object
-    async session({ session }) {
+    async session({
+      session,
+      user,
+    }: {
+      session: any;
+      user: NextAuthUser | AdapterUser;
+    }) {
       // 1. Get user from database
-      const user = await User.findOne({ email: session.user.email });
+      const dbUser: User | null = await User.findOne({
+        email: session.user.email,
+      });
+
       // 2. Assign the user id to the session
-      session.user.id = user._id.toString();
+      session.user.id = dbUser?._id.toString();
+
       // 3. return session
       return session;
     },
